1d38083a64ce0bc3b5d0242349ea026e84d7c809
Alvin Mukiibi <mukikelly792@gmail.com>
2020-04-04 19:14:27 +0300

    logic in ts 1

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { MovieService, SearchType } from "./../../services/movie.service";
+import { Component, OnInit } from "@angular/core";
 
+MovieService;
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.page.html',
-  styleUrls: ['./movies.page.scss'],
+  selector: "app-movies",
+  templateUrl: "./movies.page.html",
+  styleUrls: ["./movies.page.scss"],
 })
 export class MoviesPage implements OnInit {
+  results: Observable<any>;
+  searchTerm: string = "";
+  type: SearchType = SearchType.all;
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  searchChanged() {
+    // call our service function which returns an observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
   }
-
 }

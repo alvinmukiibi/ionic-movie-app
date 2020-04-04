07021a194f1c1c701526819394680477d47f7685
Alvin Mukiibi <mukikelly792@gmail.com>
2020-04-04 19:05:18 +0300

    define service with api calls

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class MovieService {
+export enum SearchType {
+  all = "",
+  movie = "movie",
+  series = "series",
+  episode = "episode",
+}
 
-  constructor() { }
+export class MovieService {
+  url = "http://www.omdbapi.com/";
+  apiKey = "a4b08741";
+
+  constructor(private http: HttpClient) {}
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    let api = `${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${
+      this.apiKey
+    }`;
+
+    let result = this.http.get(api).pipe(map((res) => res["Search"]));
+
+    return result;
+  }
+
+  getDetails(id) {
+    let api = `${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`;
+
+    let result = this.http.get(api);
+
+    return result;
+  }
 }
